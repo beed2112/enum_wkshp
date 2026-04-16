36d9dd60398d545db16021581de8093deaa6077d
beed2112 <32287825+beed2112@users.noreply.github.com>
2026-04-15 17:00:28 -0700

    Update mysql_setup.sql

--- lab/mysql_setup.sql
+++ lab/mysql_setup.sql
@@ -1,4 +1,21 @@
-CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '';
+CREATE DATABASE IF NOT EXISTS workshop;
+
+CREATE USER IF NOT EXISTS 'mysql'@'%' IDENTIFIED WITH mysql_native_password BY '';
+GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+USE workshop;
+
+CREATE TABLE IF NOT EXISTS flags (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  flag VARCHAR(255) NOT NULL
+);
+
+INSERT INTO flags(flag)
+SELECT 'enum-wkshp{mysql_login_successful}'
+WHERE NOT EXISTS (
+  SELECT 1 FROM flags WHERE flag='enum-wkshp{mysql_login_successful}'
+);CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
 FLUSH PRIVILEGES;
 USE workshop;
