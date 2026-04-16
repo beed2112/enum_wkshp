1882e9480bef9431116e7aabfc39923491bf8bfe
beed2112 <32287825+beed2112@users.noreply.github.com>
2026-04-16 07:04:07 -0700

    mysql changes

--- lab/mysql_setup.sql
+++ lab/mysql_setup.sql
@@ -15,14 +15,4 @@ INSERT INTO flags(flag)
 SELECT 'enum-wkshp{mysql_login_successful}'
 WHERE NOT EXISTS (
   SELECT 1 FROM flags WHERE flag='enum-wkshp{mysql_login_successful}'
-);CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
-FLUSH PRIVILEGES;
-USE workshop;
-CREATE TABLE IF NOT EXISTS flags (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  flag VARCHAR(255) NOT NULL
 );
-INSERT INTO flags(flag)
-SELECT 'enum-wkshp{mysql_login_successful}'
-WHERE NOT EXISTS (SELECT 1 FROM flags WHERE flag='enum-wkshp{mysql_login_successful}');
--- lab/start.sh
+++ lab/start.sh
@@ -10,7 +10,7 @@ if [ ! -d /var/lib/mysql/mysql ]; then
   mysql_install_db --user=mysql --ldata=/var/lib/mysql || mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 fi
 
-service mariadb start || service mysql start || true
+service mariadb start || service mysql start
 sleep 5
 
 mysqladmin ping -uroot || true
